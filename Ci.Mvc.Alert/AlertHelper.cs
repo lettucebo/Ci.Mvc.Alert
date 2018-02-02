21a96aaada2c5a0534cb2e59331df932dbfff6d8
Money <abc12207@gmail.com>
2018-02-02 12:21:54 +0800

    Init.

---
+++ Ci.Mvc.Alert/AlertHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ci.Extension;
+
+namespace Ci.Mvc.Alert
+{
+    public static class AlertHelper
+    {
+        public static MvcHtmlString ShowAlert(this HtmlHelper helper)
+        {
+            var message = helper.ViewContext.Controller.TempData["CiMvcAlert"];
+
+            if (message == null || message.ToString().IsNullOrWhiteSpace())
+            {
+                return MvcHtmlString.Empty;
+            }
+
+            var script = new TagBuilder("script");
+            script.Attributes.Add("type", "text/javascript");
+            script.InnerHtml = $@"
+                                    // start CiMvcAlert
+                                    if (!(typeof bootbox === 'object')) {{
+                                        console.log('Ci.Mvc.Alert require bootboxJs to run!')
+                                    }} else {{
+                                        bootbox.alert('{message}');
+                                    }}
+                                    // end CiMvcAlert
+                                ";
+            return MvcHtmlString.Create(script.ToString());
+        }
+    }
+}
---
+++ Ci.Mvc.Alert/ControllerExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ci.Mvc.Alert
+{
+    public static class ControllerExtension
+    {
+        public static void SetAlert(this Controller controller, string message)
+        {
+            controller.TempData["CiMvcAlert"] = message;
+        }
+    }
+}
