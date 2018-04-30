63dce7016f781c47b2cdc9997679f50ded58ab32
Money <abc12207@gmail.com>
2018-04-30 12:31:47 +0800

    add aspnet core support

---
+++ Ci.Mvc.Alert.Core/AlertHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Ci.Mvc.Alert.Core
+{
+    [HtmlTargetElement("CiMvcAlert")]
+    public class CiMvcAlertTagHelper : TagHelper
+    {
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ViewContext?.TempData != null)
+            {
+                var tempData = ViewContext.TempData;
+
+                output.TagName = "script";
+
+                var script = $@"
+                                    // start CiMvcAlert
+                                    if (!(typeof bootbox === 'object')) {{
+                                        console.log('%c Ci.Mvc.Alert require bootboxJs to run!','color:#FF0000;')
+                                    }} else {{
+                                        bootbox.alert('{tempData["CiMvcAlert"]}');
+                                    }}
+                                    // end CiMvcAlert
+                                ";
+                output.PostContent.AppendHtml(script);
+            }
+        }
+    }
+}
---
+++ Ci.Mvc.Alert.Core/ControllerExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Ci.Mvc.Alert.Core
+{
+    public static class ControllerExtension
+    {
+        public static void SetAlert(this Controller controller, string message)
+        {
+            controller.TempData["CiMvcAlert"] = message;
+        }
+    }
+}
