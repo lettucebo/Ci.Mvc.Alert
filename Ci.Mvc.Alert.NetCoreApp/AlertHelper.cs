49d71cd42a485b26982bbbaab3942874b8ae5d64
Money <abc12207@gmail.com>
2019-10-05 17:49:21 +0800

    add netcore 3.0 support

---
+++ Ci.Mvc.Alert.NetCoreApp/AlertHelper.cs
@@ -0,0 +1,61 @@
+﻿using Ci.Extension;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Ci.Mvc.Alert.CoreApp
+{
+    [HtmlTargetElement("cimvcalert")]
+    public class CiMvcAlertTagHelper : TagHelper
+    {
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ViewContext?.TempData != null)
+            {
+                var tempData = ViewContext.TempData;
+
+                var msg = tempData["CiMvcAlertMsg"]?.ToString();
+                var title = tempData["CiMvcAlertTitle"]?.ToString();
+
+                if (!msg.IsNullOrWhiteSpace() || !title.IsNullOrWhiteSpace())
+                {
+                    output.TagName = "script";
+                    output.TagMode = TagMode.StartTagAndEndTag;
+                    output.Attributes.Add("type", "text/javascript");
+
+                    string alert;
+                    if (!title.IsNullOrWhiteSpace())
+                    {
+                        alert = $@"bootbox.alert( 
+                            {{
+                                title: '{title}',
+                                message: '{msg}' 
+                            }}
+                        ); ";
+                    }
+                    else
+                    {
+                        alert = $@"bootbox.alert('{msg}');";
+
+                    }
+
+                    var script = $@"
+                                    // start CiMvcAlert
+                                    if (!(typeof bootbox === 'object')) {{
+                                        throw('Ci.Mvc.Alert require bootboxJs to run!');
+                                    }} else {{
+                                        {alert}
+                                    }}
+                                    // end CiMvcAlert
+                                ";
+
+                    output.PreContent.SetHtmlContent(script);
+                }
+            }
+        }
+    }
+}
---
+++ Ci.Mvc.Alert.NetCoreApp/ControllerExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ci.Mvc.Alert.CoreApp
+{
+    public static class ControllerExtension
+    {
+        public static void SetAlert(this Controller controller, string message, string title = "")
+        {
+            controller.TempData["CiMvcAlertMsg"] = message;
+            controller.TempData["CiMvcAlertTitle"] = title;
+        }
+    }
+}
